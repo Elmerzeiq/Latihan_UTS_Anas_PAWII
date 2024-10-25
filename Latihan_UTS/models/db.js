63d79f3a2f3fad87edc25ca04ea8a8ad469d6ac8
Elmerzeiq <anasalmarzaq_2226240070@mhs.mdp.ac.id>
2024-10-25 18:46:50 +0700

    commit

---
+++ Latihan_UTS/models/db.js
@@ -0,0 +1,18 @@
+let mongoose = require('mongoose');
+let dbURI = 'mongodb://localhost:27017/db_latihan_uts';
+
+mongoose.connect(dbURI,{
+    //userNewUrlParser :true
+});
+
+mongoose.connection.on("connected", () => {
+    console.log("Connected to MongoDB")
+});
+mongoose.connection.on("error", (error) => {
+    console.log("Connection Error : " + error );
+
+});
+mongoose.connection.on("disconnected", () => {
+    console.log("Disconnected from MongoDB")
+});
+
